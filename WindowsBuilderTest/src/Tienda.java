f5ab370c94441fa77bcba5f0c48c8096175e728f
pablo <pablo@DESKTOP-SL1E8NH>
2019-05-05 13:57:19 +0200

    funicona paso de tienda a inventario

--- WindowsBuilderTest/src/Tienda.java
+++ WindowsBuilderTest/src/Tienda.java
@@ -12,8 +12,13 @@ public class Tienda extends JFrame {
 
 	private JFrame frame;
 
-	int valorObjetoUno, valorObjetoDos, valorObjetoTres;
 	int idObjetoUno, idObjetoDos, idObjetoTres;
+	String nombreObjetoUno, nombreObjetoDos, nombreObjetoTres;
+	int poderObjetoUno, poderObjetoDos, poderObjetoTres;
+	int valorObjetoUno, valorObjetoDos, valorObjetoTres;
+	String categoriaObjetoUno, categoriaObjetoDos, categoriaObjetoTres;
+	String tipoObjetoUno, tipoObjetoDos, tipoObjetoTres;
+
 	int oro = 200;
 	String consultaObjetoUno = "SELECT * FROM items ORDER BY RAND() LIMIT 1";
 	String consultaObjetoDos = "SELECT * FROM items ORDER BY RAND() LIMIT 1";
@@ -21,7 +26,7 @@ public class Tienda extends JFrame {
 
 	ArrayList<TiendaGetSet> meterRS = new ArrayList<TiendaGetSet>();
 	int sizeRStienda = meterRS.size();
-	TiendaGetSet objetoTienda = new TiendaGetSet(0, 0);
+	// TiendaGetSet objetoTienda = new TiendaGetSet(0, 0, 0, 0, 0, 0);
 
 	/**
 	 * Launch the application.
@@ -54,10 +59,15 @@ public class Tienda extends JFrame {
 
 			while (rsUno.next()) {
 
-				valorObjetoUno = rsUno.getInt("valor");
 				idObjetoUno = rsUno.getInt("id");
+				nombreObjetoUno = rsUno.getString("nombre");
+				poderObjetoUno = rsUno.getInt("poder");
+				valorObjetoUno = rsUno.getInt("valor");
+				categoriaObjetoUno = rsUno.getString("categoria");
+				tipoObjetoUno = rsUno.getString("tipo");
 
-				TiendaGetSet kUno = new TiendaGetSet(valorObjetoUno, idObjetoUno);
+				TiendaGetSet kUno = new TiendaGetSet(idObjetoUno, nombreObjetoUno, poderObjetoUno, valorObjetoUno,
+						categoriaObjetoUno, tipoObjetoUno);
 
 				meterRS.add(kUno);
 				// rsUno.close();
@@ -73,10 +83,15 @@ public class Tienda extends JFrame {
 
 			while (rsDos.next()) {
 
+				idObjetoDos = rsDos.getInt("id");
+				nombreObjetoDos = rsDos.getString("nombre");
+				poderObjetoDos = rsDos.getInt("poder");
 				valorObjetoDos = rsDos.getInt("valor");
-				idObjetoDos = rsUno.getInt("id");
+				categoriaObjetoDos = rsDos.getString("categoria");
+				tipoObjetoDos = rsDos.getString("tipo");
 
-				TiendaGetSet kDos = new TiendaGetSet(valorObjetoDos, idObjetoDos);
+				TiendaGetSet kDos = new TiendaGetSet(idObjetoDos, nombreObjetoDos, poderObjetoDos, valorObjetoDos,
+						categoriaObjetoDos, tipoObjetoDos);
 
 				meterRS.add(kDos);
 				// rsUno.close();
@@ -92,10 +107,15 @@ public class Tienda extends JFrame {
 
 			while (rsTres.next()) {
 
+				idObjetoTres = rsTres.getInt("id");
+				nombreObjetoTres = rsTres.getString("nombre");
+				poderObjetoTres = rsTres.getInt("poder");
 				valorObjetoTres = rsTres.getInt("valor");
-				idObjetoTres = rsUno.getInt("id");
+				categoriaObjetoTres = rsTres.getString("categoria");
+				tipoObjetoTres = rsTres.getString("tipo");
 
-				TiendaGetSet kTres = new TiendaGetSet(valorObjetoTres, idObjetoTres);
+				TiendaGetSet kTres = new TiendaGetSet(idObjetoTres, nombreObjetoTres, poderObjetoTres, valorObjetoTres,
+						categoriaObjetoTres, tipoObjetoTres);
 
 				meterRS.add(kTres);
 				// rsUno.close();
@@ -109,25 +129,49 @@ public class Tienda extends JFrame {
 		JButton btnNewButton = new JButton("COMPRAR OBJETO");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				if (oro >= valorObjetoUno ) {
-					
+				if (oro >= valorObjetoUno) {
+					AddBaseDeDatos(idObjetoUno, nombreObjetoUno, poderObjetoUno, valorObjetoUno, categoriaObjetoUno,
+							tipoObjetoUno);
 				} else {
 					System.out.println("NO TIENES EL ORO PARA COMPRAR ESE OBJETO");
+					System.out.println("Tu oro actual: " + oro);
 				}
 			}
 
 		});
-		btnNewButton.setBounds(155, 293, 89, 23);
+		btnNewButton.setBounds(119, 293, 147, 23);
 		getContentPane().add(btnNewButton);
 
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(155, 327, 89, 23);
+		JButton btnNewButton_1 = new JButton("COMPRAR OBJETO");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (oro >= valorObjetoDos) {
+					AddBaseDeDatos(idObjetoDos, nombreObjetoDos, poderObjetoDos, valorObjetoDos, categoriaObjetoDos,
+							tipoObjetoDos);
+				} else {
+					System.out.println("NO TIENES EL ORO PARA COMPRAR ESE OBJETO");
+					System.out.println("Tu oro actual: " + oro);
+				}
+			}
+
+		});
+		btnNewButton_1.setBounds(119, 327, 147, 23);
 		getContentPane().add(btnNewButton_1);
 
+		JButton btnNewButton_2 = new JButton("COMPRAR OBJETO");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (oro >= valorObjetoTres) {
+					AddBaseDeDatos(idObjetoTres, nombreObjetoTres, poderObjetoTres, valorObjetoTres,
+							categoriaObjetoTres, tipoObjetoTres);
+				} else {
+					System.out.println("NO TIENES EL ORO PARA COMPRAR ESE OBJETO");
+					System.out.println("Tu oro actual: " + oro);
+				}
+			}
 
-
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(155, 363, 89, 23);
+		});
+		btnNewButton_2.setBounds(119, 363, 147, 23);
 		getContentPane().add(btnNewButton_2);
 
 		JLabel lblTiraParaVer = new JLabel("TIRA PARA VER SI HAY LEGENDARIO");
@@ -135,11 +179,11 @@ public class Tienda extends JFrame {
 		getContentPane().add(lblTiraParaVer);
 
 		JLabel lblTuOro = new JLabel("TU ORO: " + oro);
-		lblTuOro.setBounds(430, 11, 119, 14);
+		lblTuOro.setBounds(361, 11, 119, 14);
 		getContentPane().add(lblTuOro);
 
 		JLabel lblBienvenidoALa = new JLabel("BIENVENIDO A LA TIENDA DE DO\u00D1A EUGENIA");
-		lblBienvenidoALa.setBounds(10, 11, 243, 14);
+		lblBienvenidoALa.setBounds(23, 11, 243, 14);
 		getContentPane().add(lblBienvenidoALa);
 
 		JButton btnNewButton_4 = new JButton("INVENTARIO");
@@ -157,11 +201,11 @@ public class Tienda extends JFrame {
 				inventarioVisible.setVisible(true);
 			}
 		});
-		btnNewButton_4.setBounds(481, 46, 89, 23);
+		btnNewButton_4.setBounds(440, 46, 130, 23);
 		getContentPane().add(btnNewButton_4);
 
 		JLabel lblInventario = new JLabel("INVENTARIO:");
-		lblInventario.setBounds(407, 50, 69, 14);
+		lblInventario.setBounds(361, 50, 69, 14);
 		getContentPane().add(lblInventario);
 
 		JLabel lblTeHaTocado = new JLabel("TE HA TOCADO: sampletext");
@@ -246,10 +290,6 @@ public class Tienda extends JFrame {
 		btnContinuar.setBounds(234, 681, 89, 23);
 		getContentPane().add(btnContinuar);
 
-		JLabel lblTiraElDado = new JLabel("TIRA EL DADO");
-		lblTiraElDado.setBounds(180, 11, 79, 14);
-		getContentPane().add(lblTiraElDado);
-
 		JPanel topPanel = new JPanel();
 
 		// final JLabel label = new JLabel("Hello World"); //
@@ -269,7 +309,7 @@ public class Tienda extends JFrame {
 
 				// Dado Tirada = new Dado();//
 				// Tirada.tirarDado(6);//
-				JOptionPane.showMessageDialog(dado.getComponent(0), "Has sacado un " + tirada); 
+				JOptionPane.showMessageDialog(dado.getComponent(0), "Has sacado un " + tirada);
 
 			}
 		});
@@ -277,6 +317,93 @@ public class Tienda extends JFrame {
 		btnNewButton_3.setBounds(234, 540, 89, 23);
 		getContentPane().add(btnNewButton_3);
 
+		JLabel lblNewLabel = new JLabel("[ " +idObjetoUno + " " +nombreObjetoUno + " " + poderObjetoUno + " " + valorObjetoUno + " " + categoriaObjetoUno +
+			" " +	tipoObjetoUno + " ]");
+		
+
+		lblNewLabel.setBounds(302, 297, 229, 14);
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(302, 331, 46, 14);
+		getContentPane().add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(302, 367, 46, 14);
+		getContentPane().add(lblNewLabel_2);
+
+	}
+
+	public int getIdObjetoUno() {
+		return idObjetoUno;
+	}
+
+	public void setIdObjetoUno(int idObjetoUno) {
+		this.idObjetoUno = idObjetoUno;
+	}
+
+	public int getIdObjetoDos() {
+		return idObjetoDos;
+	}
+
+	public void setIdObjetoDos(int idObjetoDos) {
+		this.idObjetoDos = idObjetoDos;
+	}
+
+	public int getIdObjetoTres() {
+		return idObjetoTres;
+	}
+
+	public void setIdObjetoTres(int idObjetoTres) {
+		this.idObjetoTres = idObjetoTres;
+	}
+
+	public String getNombreObjetoUno() {
+		return nombreObjetoUno;
+	}
+
+	public void setNombreObjetoUno(String nombreObjetoUno) {
+		this.nombreObjetoUno = nombreObjetoUno;
+	}
+
+	public String getNombreObjetoDos() {
+		return nombreObjetoDos;
+	}
+
+	public void setNombreObjetoDos(String nombreObjetoDos) {
+		this.nombreObjetoDos = nombreObjetoDos;
+	}
+
+	public String getNombreObjetoTres() {
+		return nombreObjetoTres;
+	}
+
+	public void setNombreObjetoTres(String nombreObjetoTres) {
+		this.nombreObjetoTres = nombreObjetoTres;
+	}
+
+	public int getPoderObjetoUno() {
+		return poderObjetoUno;
+	}
+
+	public void setPoderObjetoUno(int poderObjetoUno) {
+		this.poderObjetoUno = poderObjetoUno;
+	}
+
+	public int getPoderObjetoDos() {
+		return poderObjetoDos;
+	}
+
+	public void setPoderObjetoDos(int poderObjetoDos) {
+		this.poderObjetoDos = poderObjetoDos;
+	}
+
+	public int getPoderObjetoTres() {
+		return poderObjetoTres;
+	}
+
+	public void setPoderObjetoTres(int poderObjetoTres) {
+		this.poderObjetoTres = poderObjetoTres;
 	}
 
 	public int getValorObjetoUno() {
@@ -303,6 +430,54 @@ public class Tienda extends JFrame {
 		this.valorObjetoTres = valorObjetoTres;
 	}
 
+	public String getCategoriaObjetoUno() {
+		return categoriaObjetoUno;
+	}
+
+	public void setCategoriaObjetoUno(String categoriaObjetoUno) {
+		this.categoriaObjetoUno = categoriaObjetoUno;
+	}
+
+	public String getCategoriaObjetoDos() {
+		return categoriaObjetoDos;
+	}
+
+	public void setCategoriaObjetoDos(String categoriaObjetoDos) {
+		this.categoriaObjetoDos = categoriaObjetoDos;
+	}
+
+	public String getCategoriaObjetoTres() {
+		return categoriaObjetoTres;
+	}
+
+	public void setCategoriaObjetoTres(String categoriaObjetoTres) {
+		this.categoriaObjetoTres = categoriaObjetoTres;
+	}
+
+	public String getTipoObjetoUno() {
+		return tipoObjetoUno;
+	}
+
+	public void setTipoObjetoUno(String tipoObjetoUno) {
+		this.tipoObjetoUno = tipoObjetoUno;
+	}
+
+	public String getTipoObjetoDos() {
+		return tipoObjetoDos;
+	}
+
+	public void setTipoObjetoDos(String tipoObjetoDos) {
+		this.tipoObjetoDos = tipoObjetoDos;
+	}
+
+	public String getTipoObjetoTres() {
+		return tipoObjetoTres;
+	}
+
+	public void setTipoObjetoTres(String tipoObjetoTres) {
+		this.tipoObjetoTres = tipoObjetoTres;
+	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -317,7 +492,7 @@ public class Tienda extends JFrame {
 		Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(winClosingEvent);
 	}
 
-	public void AddBaseDeDatos(String id, String nombre, String poder, String valor, String categoria, String tipo) {
+	public void AddBaseDeDatos(int id, String nombre, int poder, int valor, String categoria, String tipo) {
 
 		Conexion.conectar();
 
--- WindowsBuilderTest/src/TiendaGetSet.java
+++ WindowsBuilderTest/src/TiendaGetSet.java
@@ -1,19 +1,19 @@
 public class TiendaGetSet {
-	private int valor;
 	private int id;
-	
-	public TiendaGetSet(int valor, int id) {
+	private String nombre;
+	private int poder;
+	private int valor;
+	private String categoria;
+	private String tipo;
+
+	public TiendaGetSet(int id, String nombre, int poder, int valor, String categoria, String tipo) {
 		super();
-		this.valor = valor;
 		this.id = id;
-	}
-
-	public int getValor() {
-		return valor;
-	}
-
-	public void setValor(int valor) {
+		this.nombre = nombre;
+		this.poder = poder;
 		this.valor = valor;
+		this.categoria = categoria;
+		this.tipo = tipo;
 	}
 
 	public int getId() {
@@ -24,15 +24,50 @@ public class TiendaGetSet {
 		this.id = id;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPoder() {
+		return poder;
+	}
+
+	public void setPoder(int poder) {
+		this.poder = poder;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
 	@Override
 	public String toString() {
-		return "TiendaGetSet [valor=" + valor + ", id=" + id + "]";
+		return "TiendaGetSet [id=" + id + ", nombre=" + nombre + ", poder=" + poder + ", valor=" + valor
+				+ ", categoria=" + categoria + ", tipo=" + tipo + "]";
 	}
-	
-	
-	
-
-	
-	
 
 }
